00c943625b5432c081ed018fe00c4bb983ccfa33
vmolina <vbellonbcn@gmail.com>
2016-04-06 10:45:38 +0200

    Authorization api updated

--- fortalesa/api/resources.py
+++ fortalesa/api/resources.py
@@ -1,9 +1,19 @@
 from tastypie.resources import ModelResource
 from fortalesa.models import Casteller, Event, EventType
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import DjangoAuthorization
 
 class CastellerResource(ModelResource):
     class Meta:
         queryset = Casteller.objects.all()
         resource_name = 'casteller'
         #allowed_methods = ['get']
+        authorization = DjangoAuthorization()
 
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'auth/user'
+        excludes = ['email', 'password', 'is_superuser']
+        # Add it here.
+        authentication = ApiKeyAuthentication()
